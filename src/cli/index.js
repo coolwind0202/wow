cdbaf0949c46d53e6bf977f44449b7efb338fda3
coolwind0202 <tororo_shidoro@yahoo.co.jp>
2023-12-26 16:54:31 +0900

    [feat] Add server script

---
+++ src/cli/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const syncDelivered = require("./lib/delivery/syncDelivered");
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+// 下層ディレクトリからindex.jsを実行するとどうなる？
+app.use('/api/files/local', express.static('local'));
+app.use('/api/filesdelivered', express.static('delivered'));
+
+app.post('/api/deliver', (req, res) => {
+    syncDelivered();
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
+
