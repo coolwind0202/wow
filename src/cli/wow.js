6b72c5b3a46c0786586da5367276b4621dc43efc
coolwind0202 <tororo_shidoro@yahoo.co.jp>
2023-12-26 19:13:21 +0900

    [feat] implement CLI init feature

---
+++ src/cli/wow.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+/*
+  Implemented with reference to:
+  - https://yargs.js.org/
+  - https://nodejs.org/api/process.html#processcwd
+*/
+
+const path = require("path");
+const createInitializedDir = require('./lib/init/createInitializedDir');
+
+require('yargs')
+  .scriptName("wow")
+  .usage('$0 <cmd> [args]')
+  .command('init [template]', 'Initialize current dircetory.', {
+    template: {
+      type: "string",
+      describe: "(option) Template directory path. CLI copies from the directory",
+      normalize: true
+    }
+  }, function (argv) {
+    console.log(`Initialize this directory: ${process.cwd()}\n`);
+
+    if (argv.template != undefined) {
+      console.log(`Template specified: ${argv.template}`);
+    } else {
+      console.log(`Use default template: ${path.resolve(__dirname, "./template")}`);
+    }
+
+    createInitializedDir(process.cwd(), {
+      options: argv.template
+    });
+  })
+  .command("start", "Start wow server.", {
+    port: {
+      describe: "Port number you want to bind the server to",
+      number: true
+    }
+  }, (argv) => {
+    // start wow server.
+  })
+  .command("generate", "Generate files to local directory.", (yargs) => {
+    yargs.command("component", "Generate component.", {
+      name: {
+        describe: "Component name"
+      }
+    }, (argv) => {
+      // create component template to `local/name` dir.
+    });
+  })
+  .help()
+  .argv;
