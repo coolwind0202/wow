b4f33ff904cfe5fbff1f831dcf3b168ed5398217
coolwind0202 <tororo_shidoro@yahoo.co.jp>
2023-12-29 00:56:25 +0900

    [feat] implement POST /api/deliver

--- develop/packages/server/server.js
+++ develop/packages/server/server.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import path from "path";
+
+import copySourceDir from "file_util/src/copySourceDir/index.js";
+import { PathIsDirError, PathNotExistsError } from 'file_util/src/copySourceDir/pathAccessErrors.js';
 
 const PORT = 3000;
 
@@ -8,8 +12,19 @@ const app = express();
   write routes here except web frontend.
 */
 
-app.get("hello", (req, res) => {
-  res.send("world");
+app.post("/api/deliver", (req, res) => {
+  const cwd = process.cwd();
+  const local = path.resolve(cwd, "local");
+  const delivery = path.resolve(cwd, "delivery");
+  try {
+    copySourceDir(local, delivery);
+    res.status(200);
+    res.send("Copying local directory has succeeded.");
+  } catch (e) {
+    res.status(500);
+    console.error(e);
+    res.send("Copying local directory has failed. Please check the console.");
+  }
 });
 
 app.listen(PORT, () => {
--- develop/packages/web/devServer.js
+++ develop/packages/web/devServer.js
@@ -1,15 +1,24 @@
 import { app } from "server/server.js";
 import { createServer as createViteServer } from "vite";
 
+/* for resolving __dirname */
+import path from "node:path";
+import url from "node:url";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 async function createServer() {
   // ミドルウェアモードで Vite サーバーを作成
   const vite = await createViteServer({
     server: { middlewareMode: true },
     appType: "mpa", // ViteのデフォルトのHTMLを処理するミドルウェアを含めない
+    root: __dirname
   });
   app.use((req, res, next) => {
     vite.middlewares.handle(req, res, next);
   });
+  console.log("[debug] create server end");
 }
 
 createServer();
